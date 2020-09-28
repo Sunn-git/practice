3f8bbd1d7a695ca880e3af63bff1acf9ea4553db
geranium236 <geranium236@gmail.com>
2020-09-28 21:59:03 +0900

    자릿수 더하기

--- Programmers/src/Add_Digits/Add_Digits_1sh.java
+++ Programmers/src/Add_Digits/Add_Digits_1sh.java
@@ -1,16 +1,22 @@
 package Add_Digits;
 
-public class Add_Digits_1sh {
+public class Add_Digits_1sh { //(0.03ms, 54.5MB)
 
 	public static void main(String[] args) {
-		int n = 123; // 6
+//		int n = 123; // 6
 		
-//		int n = 987; // 24
+		int n = 987; // 24
 		
 		int answer = 0;
 		
+		while(n > 0) {
+			answer += (n % 10);
+			n /= 10; 
+		};
 		
-//		System.out.println(answer);
+		
+		
+		System.out.println(answer);
 		
 
 	}
