60e020574b98657cdc6cdde49a4990eadec6f372
geranium236 <geranium236@gmail.com>
2020-10-21 21:58:20 +0900

    타겟 넘버. DFS / BFS 구글링 했음. 아직 공부가 더 필요하다

---
+++ Programmers/src/TargetNum/Permutation.java
@@ -0,0 +1,43 @@
+package TargetNum;
+
+public class Permutation {
+	public static void main(String[] args) {
+		int[] numbers = {1, 1, 1, 1, 1};
+		int target = 3;
+		int[] input = {1, -1};
+		
+//		int n = input.length;
+//		int r = numbers.length;
+		int[] answer = new int[numbers.length];
+		
+		permutationDup(input, numbers, answer, 0);
+		
+//		System.out.println();
+		
+	}
+
+	public static void permutationDup
+	(int[] input, int[] numbers, int answer[], int depth) {
+		if(depth == numbers.length) {
+			print(answer);
+			return;
+		}
+		
+		for(int i = 0; i < input.length; i++) {
+			answer[depth] = input[i] * numbers[depth];
+			permutationDup(input, numbers, answer, depth+1);
+		}
+		
+	}
+	
+	public static void print(int[] answer) {
+		int sum = 0;
+		for(Integer ans : answer) {
+			System.out.print(ans + " ");
+			sum += ans;
+		}
+		System.out.print(" = " + sum);
+		System.out.println();
+
+	}
+}
---
+++ Programmers/src/TargetNum/TargetNum_1st.java
@@ -0,0 +1,34 @@
+package TargetNum;
+
+public class TargetNum_1st {
+	
+	public static void main(String[] args) {
+		int[] numbers = {1,1,1,1,1};
+		int target = 3;
+		
+//		int[] numbers = {2, 3, 5, 7, 9};
+//		int target = 2;
+		
+//		int[] sign = {1, -1};
+		
+		
+		int answer = 0;
+		
+		answer = dfs(numbers, 0, 0, target);
+
+
+		System.out.println(answer);
+	}
+
+	private static int dfs(int[] numbers, int depth, int sum, int target) {
+		if(depth == numbers.length) {
+			if(sum == target) {
+				return 1;
+			}
+			return 0;
+		}
+		return dfs(numbers, depth+1, sum + numbers[depth], target) 
+				+ dfs(numbers, depth+1, sum - numbers[depth], target);
+	}
+	
+}
