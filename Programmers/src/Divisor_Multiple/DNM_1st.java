b1168d23390a36e0d7b06ff67b15570428e221bf
geranium236 <geranium236@gmail.com>
2020-11-01 23:04:11 +0900

    최대공약수와 최소공배수 시작

---
+++ Programmers/src/Divisor_Multiple/DNM_1st.java
@@ -0,0 +1,20 @@
+package Divisor_Multiple;
+
+public class DNM_1st {
+
+	public static void main(String[] args) {
+		int n = 3;
+		int m = 12; // {3,12}
+
+//		int n = 2;
+//		int m = 5; // {1, 10}
+		
+		
+		int[] answer = new int[2];
+		
+		for(int i : answer) {
+			System.out.printf("%d, ", i);
+		}
+	}
+
+}
