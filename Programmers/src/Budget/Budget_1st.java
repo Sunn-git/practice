5a1e372edcea4e52570736ebbbb9a160cfeff58b
geranium236 <geranium236@gmail.com>
2020-11-03 20:04:58 +0900

    예산

---
+++ Programmers/src/Budget/Budget_1st.java
@@ -0,0 +1,30 @@
+package Budget;
+
+import java.util.Arrays;
+
+public class Budget_1st { 
+
+	public static void main(String[] args) {
+//		int[] d = {1,3,2,5,4};
+//		int budget = 9; //3
+		
+		int[] d = {2,2,3,3};
+		int budget = 10; //4
+		
+		int answer = 0;
+		
+		Arrays.sort(d);
+
+        for (int i = 0; i < d.length; i++) {
+            budget -= d[i];
+
+            if (budget < 0) break; // 이렇게 하는게 훨씬 간단하다
+
+            answer++;
+        }
+		
+		System.out.println(answer);
+
+	}
+
+}
---
+++ Programmers/src/Budget/Budget_2nd_others.java
@@ -0,0 +1,35 @@
+package Budget;
+
+import java.util.Arrays;
+
+public class Budget_2nd_others { // (2.68ms, 51.9MB)
+
+	public static void main(String[] args) {
+		int[] d = {1,3,2,5,4};
+		int budget = 9; //3
+		
+//		int[] d = {2,2,3,3};
+//		int budget = 10; //4
+		
+		int answer = 0;
+		
+		Arrays.sort(d);
+		
+		for(int i = 0; i < d.length; i++) {			
+			if(budget - d[i] >= 0) {
+				budget -= d[i];
+			}else {
+				answer = i;
+				break;
+			}
+			
+			if(i == d.length-1) {
+				answer = d.length;
+			}
+		}
+		
+		System.out.println(answer);
+
+	}
+
+}
