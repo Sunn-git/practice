fe17db73df465ed6dd479e09b8e8cc6bbb7cfad6
geranium236 <geranium236@gmail.com>
2020-11-04 15:56:39 +0900

    8958 OX퀴즈

--- Beakjoon/src/OneDimensionalArray/ODA_8958_1st.java
+++ Beakjoon/src/OneDimensionalArray/ODA_8958_1st.java
@@ -5,29 +5,39 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class ODA_8958_1st {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
-		int n, temp, max = 0;
-		double sum = 0;
+		int n, total, score;
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = null;
 		
 		n = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
 		
+		int[] arr = new int[n];
+
 		for(int i = 0; i < n; i++) {
-			temp= Integer.parseInt(st.nextToken());
-			max = (temp > max) ? temp : max;
-			sum += temp;
+			String answer = br.readLine();
+			total = 0;
+			score = 1;
+			
+			for(int j = 0; j < answer.length(); j++) {
+				if(answer.charAt(j) != 'O') {
+					score = 1;
+					continue;
+				}
+				total += score++;
+			}
+			
+			arr[i] = total;
 		}
 		
-		bw.write(Double.toString((sum/max)*100/n));
+		for(int i : arr) {
+			bw.write(Integer.toString(i)+"\n");
+		}
 		
 		bw.flush();
 	}
