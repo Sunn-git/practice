1531dd7af552b62158b265fbf8ca19fc52076861
geranium236 <geranium236@gmail.com>
2020-11-10 00:52:59 +0900

    실패율 완료. >>  toArray(T[] a), Comparator

--- Programmers/src/FailureRate/FailureRate_2nd.java
+++ Programmers/src/FailureRate/FailureRate_2nd.java
@@ -1,21 +1,24 @@
 package FailureRate;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 
 public class FailureRate_2nd {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { // (9.89ms, 63MB)
 		int N = 5;
 		int[] stages = {2, 1, 2, 6, 2, 4, 3, 3}; // 3,4,2,1,5
 		
 //		int N = 4;
 //		int[] stages = {4,4,4,4,4}; // 4,1,2,3
 		
-		double[] failureR = new double[N];
-		int[] stay = new int[N+1];
-		int[] pass = new int[N+1];
 		
+		HashMap<Integer, Double> fr = new HashMap<Integer, Double>();
+		int[] stay = new int[N+1];
 		double total = stages.length;
+		
+		int[] answer = new int[N];
 		
 		
 		for(int i = 0; i < stages.length; i++) {
@@ -23,17 +26,61 @@ public class FailureRate_2nd {
 		}
 		
 		for(int i = 0; i < stay.length-1; i++) {
-			failureR[i] = stay[i] / total;
+			
+			if(total == 0) { // 1,6,7,9,13,23,24,25 테스트케이스 
+				fr.put(i, (double) 0);
+				continue;
+			}
+			
+			fr.put(i, stay[i] / total);
 			total -= stay[i];
 		}
 		
-		Arrays.sort(failureR);
+		Integer[] keyArr = new Integer[N];
+		fr.keySet().toArray(keyArr); 
+
+//		for(int i : keyArr) {
+//			System.out.println(i);
+//		}
 		
-		for(double d : failureR) {
-			System.out.println(d);
+		
+		Arrays.sort(keyArr, new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return fr.get(o2).compareTo(fr.get(o1));
+			}
+		});
+		
+		int index = 0;
+		
+		for(int i : keyArr) {
+			answer[index++] = i+1;
+			System.out.println(i);
 		}
 		
-
+		
+//		ArrayList<Double> keyList = new ArrayList<Double>(fr.keySet());
+//		
+//		Collections.sort(keyList, new Comparator<Double>() {
+//			@Override
+//			public int compare(Double o1, Double o2) {
+//				return fr.get(o2).compareTo(fr.get(o1));
+//			}
+//			
+//		});
+//		
+//		int index = 0;
+//		
+//		for(double d : keyList) {
+//			answer[index++] = (int)d + 1;
+//		}
+		
+		
+		
+//		for(int i : answer) {
+//			System.out.println(i);
+//		}
+		
 	}
 
 }
