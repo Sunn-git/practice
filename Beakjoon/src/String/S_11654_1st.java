57d432a70f32bd629e5f3d06fb30d1c3639a0932
geranium236 <geranium236@gmail.com>
2020-11-15 00:08:59 +0900

    아스키코드

--- Beakjoon/src/String/S_11654_1st.java
+++ Beakjoon/src/String/S_11654_1st.java
@@ -1,7 +1,25 @@
 package String;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class S_11654_1st {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String s = br.readLine();
+		int n = s.charAt(0);
+
+		bw.write(Integer.toString(n));
+		bw.flush();
+		
+		br.close();
+		bw.close();
 		
 	}
 }
