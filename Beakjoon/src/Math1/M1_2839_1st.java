37dd1f4dfb6c4c548248ea1aaf8e13664e78e2c1
geranium236 <geranium236@gmail.com>
2020-11-19 00:35:25 +0900

    설탕 배달.

--- Beakjoon/src/Math1/M1_2839_1st.java
+++ Beakjoon/src/Math1/M1_2839_1st.java
@@ -5,33 +5,26 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
-public class M1_2839_1st { // 설탕 배달 작성중
+public class M1_2839_1st { // 설탕 배달
 	public static void main(String[] args) throws IOException {
 	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int n = Integer.parseInt(br.readLine());
-		int answer = 0;
+		int answer = 5000;
 		
-		while(n >= 3) {
-			if(n % 5 == 0 || n / 5 > 0) {
-				answer += n/5;	
-				n -= (n/5)*5;
-			} else if(n % 3 == 0 || n / 3 > 0) {
-				answer += n/3;
-				n -= (n/3)*3;
-			} else {
-				break;
+		for(int i = 0; i * 5 <= n; i++) {
+			for(int j = 0; j * 3 <= n; j++) {
+				if(5*i+3*j == n) {
+//					System.out.printf("i : %d, j : %d?\n", i, j);
+					if(i+j < answer) answer = i+j;
+				}
 			}
 		}
 		
-		
-//		System.out.println(answer);
-//		System.out.println(n);
-		if(n != 0) answer = -1;
+		answer = (answer == 5000) ? -1 : answer;
 		
 		bw.write(String.valueOf(answer));		
 		bw.flush();
