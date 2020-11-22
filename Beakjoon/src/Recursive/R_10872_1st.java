8888baa82a2911817e4f89af57ae95cbab13719f
geranium236 <geranium236@gmail.com>
2020-11-22 23:59:17 +0900

    패기토리얼 시작 왜 자꾸 시간초과가 나지?

---
+++ Beakjoon/src/Recursive/R_10872_1st.java
@@ -0,0 +1,30 @@
+package Recursive;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class R_10872_1st {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+		
+		bw.write(Integer.toString(factorial(n)));
+		bw.flush();
+		
+		br.close();
+		bw.close();
+	}
+	
+	private static int factorial(int n) {
+		if(n == 1) return 1;
+		
+		return n * factorial(n-1);
+	}
+}
