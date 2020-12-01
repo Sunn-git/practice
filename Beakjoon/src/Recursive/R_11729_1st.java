b0aeaeebf5acc50a95d4d0110a422f636d6eff9f
geranium236 <geranium236@gmail.com>
2020-12-01 23:31:36 +0900

    하노이 탑 이동 순서 진행중

---
+++ Beakjoon/src/Recursive/R_11729_1st.java
@@ -0,0 +1,25 @@
+package Recursive;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class R_11729_1st { // 하노이 탑 이동 순서 진행중.. 이걸 재귀로?
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+		
+		
+		
+//		bw.write("");
+//		bw.flush();
+		
+		br.close();
+		bw.close();
+	}
+}
