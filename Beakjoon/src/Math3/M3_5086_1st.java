4f3fbf786ca4f6df25df639138922d6efba46cf9
geranium236 <geranium236@gmail.com>
2020-12-18 23:59:30 +0900

    배수와 약수 진행중.

---
+++ Beakjoon/src/Math3/M3_5086_1st.java
@@ -0,0 +1,37 @@
+package Math3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class M3_5086_1st { // 배수와 약수
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuffer sb = new StringBuffer();
+		
+		while(st.hasMoreTokens()) {
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			if(a == 0 && b == 0) break;
+			String result = "neither";
+			
+			if(a <= b) {
+				if(b % a == 0) result = "factor";
+			}else {
+				if(a % b == 0) result = "multiple";
+			}
+			sb.append(result).append("\n");
+			
+			st = new StringTokenizer(br.readLine());
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+	}
+}
