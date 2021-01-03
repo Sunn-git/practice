6eb83aec6ce0e26cb8cd357615c36da1b3da5f16
geranium236 <geranium236@gmail.com>
2021-01-03 20:16:01 +0900

    통계학 완료. 드디어 런타임에러에서 벗어남.

--- Beakjoon/src/Sort/S_2108_3rd.java
+++ Beakjoon/src/Sort/S_2108_3rd.java
@@ -5,11 +5,10 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
 
-public class S_2108_3rd { // 통계학 진행중.. 런타임 에러 이클립스에선 잘 실행되는데 왜지..
+public class S_2108_3rd { // 통계학 완료 !!!!!!!!!
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -18,16 +17,47 @@ public class S_2108_3rd { // 통계학 진행중.. 런타임 에러 이클립스
 		int n = Integer.parseInt(br.readLine());
 		int[] input = new int[n];
 		
-		int max = 0;
+		double sum = 0;
 		
 		for(int i = 0; i < n; i++) {
 			input[i] = Integer.parseInt(br.readLine());
-			if(input[i]  > max) max = input[i];
-			sb.append(input[i]);
+			sum += input[i];
 		}
 		
-
+		Arrays.sort(input);
+		int[] cnt = new int[n];
+		int num = input[0]-1; // 최소값보다 작게
+		int idx = 0;
+		for(int i = 0; i < n; i++) {
+			if(num != input[i]) {
+				idx = i;
+				num = input[i];
+			}
+			cnt[idx]++;
+		}
 		
+//		for(int i : cnt) {
+//			System.out.printf("%d, ", i);
+//		}
+//		System.out.println();
+		
+		int max = Integer.MIN_VALUE; //최대 반복 횟수 구하기
+		for(int i : cnt) {
+			if(i > max) max = i;
+		}
+		
+		//최빈값 구하기
+		ArrayList<Integer> freq = new ArrayList<Integer>();
+		for(int i = 0; i < n; i++) { 
+			if(cnt[i] == max) freq.add(input[i]);
+		}
+		
+		int freqNum = (freq.size() == 1)? freq.get(0) : freq.get(1);
+		
+		sb.append(Math.round(sum/n)).append("\n")
+			.append(input[n/2]).append("\n") //은 홀수
+			.append(freqNum).append("\n")
+			.append(input[n-1]-input[0]);
 		
 		bw.write(sb.toString());			
 		bw.flush();
