3d34e4e40f482e4e3b9db2559e01851f6d40253b
geranium236 <geranium236@gmail.com>
2021-01-15 23:55:02 +0900

    reverseInParentheses 진행중.  괄호가 따로 두 개 나오는 경우 아직 처리 안함.

---
+++ CodeSignal/src/Arcade/Intro13_reverseInParentheses_1st.java
@@ -0,0 +1,28 @@
+package Arcade;
+
+public class Intro13_reverseInParentheses_1st { 
+	public static void main(String[] args) {
+//		String inputString = "foo(bar(baz))blim";
+		String inputString = "foo(bar)baz(blim)";
+		int startI = 0;
+		int endI = 0;
+		StringBuffer temp = new StringBuffer();
+		
+		while(inputString.contains("(")) {
+			startI = inputString.lastIndexOf('(');
+			endI = inputString.indexOf(')');
+			
+//			System.out.printf("%d %d\n", startI, endI);
+			temp.setLength(0);
+			temp.append(inputString.substring(startI+1, endI));
+//			System.out.println(temp.toString());
+			
+			inputString = inputString.substring(0, startI)
+					    + temp.reverse().toString()
+					    + inputString.substring(endI+1);
+//			System.out.println(inputString);
+		}
+		
+    	System.out.println(inputString);
+	}
+}
