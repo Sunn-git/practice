046099b9f9d4ec4bd228c7e57797ec466c5d6f5f
geranium236 <geranium236@gmail.com>
2021-01-30 17:19:37 +0900

    isDigit. Character.isDigit(char c)

---
+++ CodeSignal/src/Arcade/Intro48_isDigit_1st.java
@@ -0,0 +1,23 @@
+package Arcade;
+
+public class Intro48_isDigit_1st { 
+	public static void main(String[] args) {
+		Intro48_isDigit_1st thisClass = new Intro48_isDigit_1st();
+		
+		
+	}
+	
+	boolean isDigit(char symbol) {
+	    return Character.isDigit(symbol);
+	}
+	
+	boolean isDigit2(char symbol) {
+	    return '0' <= symbol && symbol <= '9';
+	}
+	
+}
+
+/*
+
+
+*/
