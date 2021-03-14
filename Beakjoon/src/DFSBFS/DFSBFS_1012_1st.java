07941fb081115c7ae0e63f64c1a9fd14e8821a98
geranium236 <geranium236@gmail.com>
2021-03-14 23:59:10 +0900

    유기농 배추 진행중.

---
+++ Beakjoon/src/DFSBFS/DFSBFS_1012_1st.java
@@ -0,0 +1,38 @@
+package DFSBFS;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class DFSBFS_1012_1st { // 유기농 배추
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+
+		
+		
+		bw.write("");
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+
+
+}
+
+
+/*
+ 
+
+
+ */
